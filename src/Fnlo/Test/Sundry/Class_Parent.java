eb68e540c637912605aac46e55ed9ffec10cd782
Fnlo <fenglu@highgo.com>
2019-05-23 15:30:58 +0800

    Sundry...  Java Matcher etc..

---
+++ src/Fnlo/Test/Sundry/Class_Parent.java
@@ -0,0 +1,21 @@
+package Fnlo.Test.Sundry;
+
+
+/**
+ * @author 31228
+ *
+ *	[https://snailclimb.gitee.io/javaguide/#/./java/Java%E5%9F%BA%E7%A1%80%E7%9F%A5%E8%AF%86?id=_11-java-%E9%9D%A2%E5%90%91%E5%AF%B9%E8%B1%A1%E7%BC%96%E7%A8%8B%E4%B8%89%E5%A4%A7%E7%89%B9%E6%80%A7-%E5%B0%81%E8%A3%85-%E7%BB%A7%E6%89%BF-%E5%A4%9A%E6%80%81]
+ */
+public class Class_Parent {
+
+	private String name ;
+	
+	public Class_Parent(){
+		
+	}
+	
+	public Class_Parent(String name){
+		this.name = name;
+	}
+	
+}
